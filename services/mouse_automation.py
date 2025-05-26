358d6237cf8578e71afe1c8a9de571afdc44d3bb
yokamoto5742-h <yokamoto5742@gmail.com>
2025-05-26 14:03:35 +0900

    mouse_automation.pyの更新

--- services/mouse_automation.py
+++ services/mouse_automation.py
@@ -1,17 +1,19 @@
-import subprocess
 import os
+import subprocess
 
 from utils.config_manager import load_config
 
 
 def soap_copy():
     config = load_config()
+
     try:
         exe_path = config.get('Paths', 'soap_copy_file_path')
         if os.path.exists(exe_path):
             subprocess.run([exe_path], check=True)
         else:
             print(f"エラー: ファイルが見つかりません: {exe_path}")
+
     except subprocess.CalledProcessError as e:
         print(f"SOAPコピーEXE実行エラー: {e}")
     except Exception as e:
@@ -20,12 +22,14 @@ def soap_copy():
 
 def run_mouse_operation():
     config = load_config()
+
     try:
         exe_path = config.get('Paths', 'operation_file_path')
         if os.path.exists(exe_path):
             subprocess.run([exe_path], check=True)
         else:
             print(f"エラー: ファイルが見つかりません: {exe_path}")
+
     except subprocess.CalledProcessError as e:
         print(f"マウス操作EXE実行エラー: {e}")
     except Exception as e:
@@ -37,7 +41,3 @@ def main(operation_type=None):
         soap_copy()
     else:
         run_mouse_operation()
-
-
-if __name__ == "__main__":
-    main()
