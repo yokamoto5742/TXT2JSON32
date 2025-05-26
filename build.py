dd98bd86f2f55012b2566aa8784f96efccc02e57
yokamoto5742-h <yokamoto5742@gmail.com>
2025-05-26 14:00:23 +0900

    build.pyの更新

--- build.py
+++ build.py
@@ -6,8 +6,6 @@ from version_manager import update_version, update_version_py
 
 
 def build_executable():
-    os.chdir(r"C:\Users\yokam\PycharmProjects\TXT2JSON32")
-
     new_version = update_version()
     update_version_py(new_version)
 
@@ -15,14 +13,16 @@ def build_executable():
         "pyinstaller",
         "--name=TXT2JSON32",
         "--windowed",
-        "--icon=assets/TXT2JSON.ico",
+        "--icon=assets/TXT2JSON32.ico",
         "main.py"
     ])
 
-    dist_dir = os.path.join("dist", "TXT2JSON")
+    dist_dir = os.path.join("dist", "TXT2JSON32")
     internal_dir = os.path.join(dist_dir, "_internal")
 
     shutil.copy("utils/config.ini", os.path.join(internal_dir, "config.ini"))
+    shutil.copy("C:\Shinseikai\TXT2JSON32\mouseoperation.exe", os.path.join(dist_dir, "mouseoperation.exe"))
+    shutil.copy("C:\Shinseikai\TXT2JSON32\soapcopy.exe", os.path.join(dist_dir, "soapcopy.exe"))
 
     print(f"Executable built successfully. Version: {new_version}")
 
